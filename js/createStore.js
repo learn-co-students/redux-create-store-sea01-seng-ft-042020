0d9146bd1d57efc8f5695f3a43c99400378c8d5c
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-10-14 15:46:34 -0700

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,44 @@
-let state;
-
+function createStore(reducer) {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
+  };
+ 
+  return {
+    dispatch,   //notice how dispatch is now being executed dispatch()
+    getState
+                //we need dispatch here because the user might need this function
+                //somewhere in the application?
+                //getState only job is to return sate and make it avaible
+  };
+};
+ 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
-
+ 
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
-
+}
+ 
+ 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
-
-dispatch({ type: '@@INIT' })
+ 
+let store = createStore(reducer) // createStore takes the reducer as an argument, why???
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+ 
+button.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
